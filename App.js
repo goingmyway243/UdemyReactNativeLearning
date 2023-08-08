f767b0bfffa9a8c50ed9b5abf1bfc1933203e011
Đăng Nguyễn Hải <goingmyway243@gmail.com>
2023-08-08 21:03:19 +0700

    Init native-features app

--- App.js
+++ App.js
@@ -1,103 +1,20 @@
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { StatusBar } from "expo-status-bar";
 
-import LoginScreen from "./screens/LoginScreen";
-import SignupScreen from "./screens/SignupScreen";
-import WelcomeScreen from "./screens/WelcomeScreen";
-import { Colors } from "./constants/styles";
-import AuthContextProvider, { AuthContext } from "./context/auth-context";
-import { useContext, useEffect, useState } from "react";
-import IconButton from "./components/ui/IconButton";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import AppLoading from "expo-app-loading";
-
-const Stack = createNativeStackNavigator();
-
-function AuthStack() {
-  return (
-    <Stack.Navigator
-      screenOptions={{
-        headerStyle: { backgroundColor: Colors.primary500 },
-        headerTintColor: "white",
-        contentStyle: { backgroundColor: Colors.primary100 },
-      }}
-    >
-      <Stack.Screen name="Login" component={LoginScreen} />
-      <Stack.Screen name="Signup" component={SignupScreen} />
-    </Stack.Navigator>
-  );
-}
-
-function AuthenticatedStack() {
-  const context = useContext(AuthContext);
-
-  return (
-    <Stack.Navigator
-      screenOptions={{
-        headerStyle: { backgroundColor: Colors.primary500 },
-        headerTintColor: "white",
-        contentStyle: { backgroundColor: Colors.primary100 },
-        headerRight: ({ tintColor }) => {
-          return (
-            <IconButton
-              color={tintColor}
-              size={24}
-              icon={"exit-outline"}
-              onPress={context.logout}
-            />
-          );
-        },
-      }}
-    >
-      <Stack.Screen name="Welcome" component={WelcomeScreen} />
-    </Stack.Navigator>
-  );
-}
-
-function Navigation() {
-  const context = useContext(AuthContext);
-
-  return (
-    <NavigationContainer>
-      {!context.isAuthenticated ? <AuthStack /> : <AuthenticatedStack />}
-    </NavigationContainer>
-  );
-}
-
-function Root() {
-  const [isTryLogin, setTryLogin] = useState(true);
-
-  const context = useContext(AuthContext);
-
-  useEffect(() => {
-    async function fetched() {
-      const token = await AsyncStorage.getItem("token");
-
-      if (token) {
-        context.authenticate(token);
-      }
-
-      setTryLogin(false);
-    }
-
-    fetched();
-  }, []);
-
-  if (isTryLogin) {
-    return <AppLoading />;
-  }
-
-  return <Navigation />;
-}
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <>
-      <StatusBar style="light" />
-      <AuthContextProvider>
-        <Root />
-      </AuthContextProvider>
-    </>
+    <View style={styles.container}>
+      <Text>Hello World!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
