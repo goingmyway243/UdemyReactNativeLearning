ae43ce63c528b04ecce13402a37833d64b3e5e66
Đăng Nguyễn Hải <goingmyway243@gmail.com>
2023-08-21 21:33:30 +0700

    Init notification-app

--- App.js
+++ App.js
@@ -1,83 +1,17 @@
-import { StatusBar } from "expo-status-bar";
-
 import { StyleSheet, Text, View } from "react-native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { NavigationContainer } from "@react-navigation/native";
-import AllPlacesScreen from "./screens/all-places-screen";
-import AddPlaceScreen from "./screens/add-place-screen";
-import IconButton from "./components/icon-button";
-import { Colors } from "./constants/Colors";
-import MapScreen from "./screens/map-screen";
-import { useEffect, useState } from "react";
-import { dbInit } from "./utils/database";
-import AppLoading from "expo-app-loading";
-import PlaceDetailScreen from "./screens/place-detail-screen";
-
-const Stack = createNativeStackNavigator();
 
 export default function App() {
-  const [isLoading, setLoading] = useState(true);
-
-  useEffect(() => {
-    dbInit().then(
-      (success) => {
-        setLoading(false);
-      },
-      (error) => {
-        console.log(error);
-      }
-    );
-  }, []);
-
-  if (isLoading) {
-    return <AppLoading />;
-  }
-
   return (
-    <>
-      <StatusBar style="auto" />
-      <NavigationContainer>
-        <Stack.Navigator
-          screenOptions={{
-            headerStyle: { backgroundColor: Colors.primary500 },
-            headerTintColor: Colors.gray700,
-            contentStyle: { backgroundColor: Colors.primary800 },
-          }}
-        >
-          <Stack.Screen
-            name="all-places"
-            component={AllPlacesScreen}
-            options={({ navigation }) => {
-              return {
-                title: "Your favorite places",
-                headerRight: ({ tintColor }) => (
-                  <IconButton
-                    icon="add"
-                    size={24}
-                    color={tintColor}
-                    onPress={() => navigation.navigate("add-place")}
-                  />
-                ),
-              };
-            }}
-          />
-          <Stack.Screen
-            name="add-place"
-            component={AddPlaceScreen}
-            options={{ title: "Add a new place" }}
-          />
-          <Stack.Screen
-            name="place-detail"
-            component={PlaceDetailScreen}
-            options={{ title: "Loading place..." }}
-          />
-          <Stack.Screen
-            name="map"
-            component={MapScreen}
-            options={{ title: "Map" }}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </>
+    <View style={styles.container}>
+      <Text>Hello World!</Text>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
--- models/place-model.js
+++
@@ -1,9 +0,0 @@
-export class Place {
-  constructor(title, imageUri, location, id) {
-    this.title = title;
-    this.imageUri = imageUri;
-    this.address = location.address;
-    this.location = { lat: location.lat, lgt: location.lgt };
-    this.id = id;
-  }
-}
